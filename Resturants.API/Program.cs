19f9805c0b686e8260ab52776b30f6b51f21f506
darkogele <darkogele@hotmail.com>
2024-03-18 13:45:25 +0100

    Add project files.

---
+++ Resturants.API/Program.cs
@@ -0,0 +1,20 @@
+using Restaurants.API.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
